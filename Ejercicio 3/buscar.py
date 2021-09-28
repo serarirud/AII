abbe6a41bdda3843254cde37cf51a709ee98537f
Sergio Arias Ruda <sergiariasruda@gmail.com>
2021-09-28 12:57:30 +0200

    Finished buscar

--- Ejercicio 3/buscar.py
+++ Ejercicio 3/buscar.py
@@ -13,11 +13,20 @@ def search(query: str, data: tuple) -> list[tuple]:
 def search_by_title(title):
     return search('SELECT ISBN, Title, Author, Year FROM Books WHERE Title LIKE ?', ('%{}%'.format(title),))
 
-def get_all_publisher() -> list[str]:
+def search_by_publisher(publisher):
+    return search('SELECT Title, Author, Publisher FROM Books WHERE Publisher LIKE ?', (publisher,))
+
+def get_all_publishers() -> list[str]:
     con = sqlite3.connect('books.db')
     cursor = con.execute('SELECT Publisher FROM Books')
-    publishers = list(cursor)
+    publishers = {publisher[0] for publisher in cursor}
     con.close()
     return publishers
 
-print(search_by_title('Lord of'))
+if __name__ == '__main__':
+    print('### Find by title ###')
+    print(search_by_title('Lord of'))
+    print('### Find by publisher ###')
+    print(search_by_publisher('Regan Books'))
+    print('### Publishers ###')
+    print(get_all_publishers())
