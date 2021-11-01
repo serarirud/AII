0e7347256d8042774c92c8c061ce0b58fcb34ba2
Sergio Arias <sergiariasruda@gmail.com>
2021-11-01 19:23:32 +0100

    Haciendo ejercicio 10

--- Ejercicio 10/search.py
+++ Ejercicio 10/search.py
@@ -1,7 +1,5 @@
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
-from whoosh.query import DateRange
-from datetime import datetime, timedelta
 import re
 
 INDEXDIR = 'Ejercicio 10/indexdir'
@@ -21,4 +19,19 @@ def buscar(campo: str, palabra: str, *campos) -> list[str]:
     
     return results
 
-print(buscar('fecha', '{20101016TO]', 'remitente', 'asunto', 'fichero')) # para buscar por fecha al poner {fecha] te busca las fechas posteriores
+def search_by_subject_or_body(palabra: str) -> list[str, str]:
+    res = set()
+    res = res.union(set(buscar('asunto', palabra, 'remitente', 'asunto')))
+    res = res.union(set(buscar('cuerpo', palabra, 'remitente', 'asunto')))
+    return list(res)
+
+def search_by_date(date: str) -> list[str, str, str]:
+    if not re.fullmatch('\\d{8}', date):
+        raise ValueError('Introduce una fecha con el formato correcto YYYYMMDD')
+    
+    return buscar('fecha', '{' + f'{date}TO]', 'remitente', 'destinatarios', 'asunto')
+
+if __name__ == '__main__':
+    print(buscar('fecha', '{20101016TO]', 'remitente', 'asunto', 'fichero')) # para buscar por fecha al poner {fecha] te busca las fechas posteriores
+    print(search_by_subject_or_body('Contrato'))
+    print(search_by_date('20101016'))
